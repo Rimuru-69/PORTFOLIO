4e5b17946b9ebddc90840dd741f7f9e04b37c6e8
Rimuru-69 <clydetensai@gmail.com>
2025-12-02 00:48:08 +0800

    Delete My Portfolio directory

--- My Portfolio/script.js
+++
@@ -1,8 +0,0 @@
-const toggle = document.getElementById('darkModeToggle');
-
-toggle.addEventListener('click', () => {
-    document.body.classList.toggle('dark-mode');
-
-    const header = document.querySelector('header');
-    header.classList.toggle('dark-mode');
-});
